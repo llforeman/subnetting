adcf450722d4928ac10a619624eb85235755f0f3
llforeman <llforeman@users.noreply.github.com>
2025-10-16 22:26:54 +0200

    Add hello_pol.py - Python script that greets Pol

---
+++ hello_pol.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Simple Python script that says hello to Pol
+"""
+
+def say_hello_to_pol():
+    """Function that prints a greeting to Pol"""
+    print("Hello, Pol!")
+    print("Hope you're having a great day!")
+
+if __name__ == "__main__":
+    say_hello_to_pol()
